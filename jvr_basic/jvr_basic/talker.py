d17466b5473cc821522f5abb4cf9bb0277ea8f93
Jorg Visch <jorgvisch@gmail.com>
2021-05-15 22:04:22 +0200

    jvr_basic talker and listener

---
+++ jvr_basic/jvr_basic/talker.py
@@ -0,0 +1,56 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+NODE_NAME = 'talker'
+TOPIC_NAME = 'talk'
+
+class Talker(Node):
+
+    def __init__(self, namespace:str):
+        super().__init__(NODE_NAME, namespace=namespace)
+        self.publisher_ = self.create_publisher(String, TOPIC_NAME, 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    theTalker = Talker(__package__)
+
+    rclpy.spin(theTalker)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    theTalker.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
