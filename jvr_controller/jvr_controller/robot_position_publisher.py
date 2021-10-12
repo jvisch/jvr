141536bb0eb638e2815c195a697cf146318536b6
Jorg Visch <jorgvisch@gmail.com>
2021-10-12 14:07:17 +0200

    Geen idee wat de wijzigingen waren, maar volgens mij werkte rviz nu wel ok

--- jvr_controller/jvr_controller/robot_position_publisher.py
+++ jvr_controller/jvr_controller/robot_position_publisher.py
@@ -27,37 +27,35 @@ class robot_position_publisher(rclpy.node.Node):
         self.br = tf2_ros.TransformBroadcaster(self)
         self.rotation = self.euler_to_quaternion(0, 0, 0)
         self.position = Vector3(x=0.0, y=0.0, z=0.0)
-        # create callback
-        # self.timer = self.create_timer(1, self.position_callback)
 
         topic_object_detected = jvr_robot.utils.topic_name(
             jvr_robot.IObjectDetector.IObjectDetector.object_detected)
+
         self.object_detected = self.create_subscription(
-            jvr_interfaces.msg.ObjectDetection, topic_object_detected, self.object_detected_callback, 10)
+            jvr_interfaces.msg.ObjectDetection,
+            topic_object_detected,
+            self.object_detected_callback, 10)
+
         self.joint_pub = self.create_publisher(
             sensor_msgs.msg.JointState, 'joint_states', 10)
 
     def object_detected_callback(self, msg: jvr_interfaces.msg.ObjectDetection):
+        # SweepSensor (servo and ultrasone as joint)
         joint_state = sensor_msgs.msg.JointState()
         joint_state.name = ["SweepSensorServo"]
-        joint_state.header.stamp = self.get_clock().now().to_msg()
+        joint_state.header.stamp = msg.object.header.stamp # self.get_clock().now().to_msg()
         joint_state.position = [msg.angle]
         self.joint_pub.publish(joint_state)
-        self.position_callback()
-
-    def position_callback(self):
-        now = self.get_clock().now().to_msg()
-        # message declarations
+        
+        # publish robot position and poses
         odom_trans = TransformStamped()
         odom_trans.header.frame_id = 'world'
         odom_trans.child_frame_id = 'chassis'
-        odom_trans.header.stamp = now
+        odom_trans.header.stamp = msg.object.header.stamp # self.get_clock().now().to_msg()
         odom_trans.transform.rotation = self.rotation
         odom_trans.transform.translation = self.position
-
         self.br.sendTransform(odom_trans)
 
-        # time.sleep(1)
 
     def euler_to_quaternion(self, roll, pitch, yaw):  # copied from ros2 tf2 tutorial
         qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - \
