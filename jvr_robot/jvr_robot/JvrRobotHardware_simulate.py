f3ea60d3f65ac3911d67c81f647a33079cb4f00c
Jorg Visch <jorgvisch@gmail.com>
2023-01-05 17:02:18 +0100

    skeleton of simulated motor

--- jvr_robot/jvr_robot/JvrRobotHardware_simulate.py
+++ jvr_robot/jvr_robot/JvrRobotHardware_simulate.py
@@ -1,13 +1,14 @@
-import jvr_robot.hardware_simulated.SG90Servo
-import jvr_robot.hardware_simulated.UltrasoneSensor
+from jvr_robot.hardware_simulated.SG90Servo import SG90Servo
+from jvr_robot.hardware_simulated.UltrasoneSensor import UltrasoneSensor
+from jvr_robot.hardware_simulated.Motor import Motor
 
 # ###########################################
 # The simulated hardware
 
 # Distance sweep servo and ultrasone sensor
-sweep_servo = jvr_robot.hardware_simulated.SG90Servo.SG90Servo()
-sweep_sensor = jvr_robot.hardware_simulated.UltrasoneSensor.UltrasoneSensor()
+sweep_servo = SG90Servo()
+sweep_sensor = UltrasoneSensor()
 
 # left and right motor
-motor_left = None
-motor_right = None
+motor_left = Motor()
+motor_right = Motor()
