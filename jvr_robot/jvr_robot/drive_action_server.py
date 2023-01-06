236d8657d5818f85ac97c98d62c7a4846cf9c621
Jorg Visch <jorgvisch@gmail.com>
2023-01-06 16:23:05 +0100

    old node removed

--- jvr_robot/jvr_robot/drive_action_server.py
+++ jvr_robot/jvr_robot/drive_action_server.py
@@ -1,12 +1,11 @@
 import rclpy
-import rclpy.action #import ActionServer
-import rclpy.node #import Node
+import rclpy.action
+import rclpy.node
 
-# from action_tutorials_interfaces.action import Fibonacci
 import jvr_interfaces.action
 import jvr_helpers.utils
 import jvr_robot.IDrive
-
+import jvr_robot.JvrRobotHardware
 
 class DriveActionServer(rclpy.node.Node):
 
@@ -14,12 +13,8 @@ class DriveActionServer(rclpy.node.Node):
         # node
         node_name = jvr_helpers.utils.node_name(self)
         super().__init__(node_name)
+        self.left_motor, self.right_motor = jvr_robot.JvrRobotHardware.get_motors(self)
 
-        # self._action_server = rclpy.action.ActionServer(
-        #     self,
-        #     Fibonacci,
-        #     'fibonacci',
-        #     self.execute_callback)
         action_name = drive_topic = jvr_helpers.utils.topic_name(jvr_robot.IDrive.IDrive.move)
         self._action_server = rclpy.action.ActionServer(
             self,
@@ -28,8 +23,17 @@ class DriveActionServer(rclpy.node.Node):
             self.execute_callback
         )
 
-    def execute_callback(self, goal_handle):
+    def execute_callback(self, goal_handle:jvr_interfaces.action.Drive.Goal):
         self.get_logger().info('Executing goal...')
+        
+        # TODO: check the incoming values (0.3 <= p <= 1.0)
+
+        # self.left_motor.move(goal_handle.left)
+        # self.left_
+        
+
+        # All succeeded
+        self.get_logger().info("goal reached")
         goal_handle.succeed()
         result = jvr_interfaces.action.Drive.Result()
         result.total_duration.sec = 42
