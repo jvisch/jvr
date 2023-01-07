fd02cd3f340ed841e6ca98f4e91a36475ec40a8e
Jorg Visch <jorgvisch@gmail.com>
2023-01-07 19:37:53 +0100

    motors (almost ok)

--- jvr_robot/jvr_robot/JvrRobotHardware.py
+++ jvr_robot/jvr_robot/JvrRobotHardware.py
@@ -5,14 +5,39 @@ from jvr_robot.hardware.hardware_simulate import *
 
 import rclpy.node
 import rclpy.duration
+import rclpy.constants
+
+import threading
+
+
 
 
 class Motors():
     def __init__(self, n: rclpy.node.Node) -> None:
         self.left , self.right = get_motors(n)
         self.clock = n.get_clock()
+        self.c = threading.Condition()
+        n.create_guard_condition
 
-    def move(self, left_power: float, right_power: float, duration: rclpy.duration.Duration):
+    def _move_motors(self, left_power: float, right_power: float):
         self.left.move(left_power)
         self.right.move(right_power)
-        self.clock.sleep_for(duration)
+
+    def _stop_motors(self):
+        self.left.stop()
+        self.right.stop()
+
+    def move(self, left_power: float, right_power: float, duration: rclpy.duration.Duration) -> bool:
+        # starts moving, return false if movement was aborted, otherwise true
+        self._move_motors(left_power, right_power)
+        # wait for timeout or external stop
+        with self.c:
+            wait_time = duration.nanoseconds / rclpy.constants.S_TO_NS
+            got_signal = self.c.wait(wait_time)
+        self._stop_motors()
+        return not got_signal 
+        
+    def stop(self):
+        self._stop_motors()
+        with self.c:
+            self.c.notify()
