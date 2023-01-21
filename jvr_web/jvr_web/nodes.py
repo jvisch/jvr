f5070866d72117a929ad88f37caad76ecba4ea77
Jorg Visch <jorgvisch@gmail.com>
2023-01-21 23:00:25 +0100

    Nodes list

--- jvr_web/jvr_web/nodes.py
+++ jvr_web/jvr_web/nodes.py
@@ -1,9 +1,22 @@
 from . import web
 
-import flask.blueprints
+import flask
 
-bp = flask.blueprints.Blueprint('nodes', __name__, url_prefix= '/nodes')
 
-@bp.route('list')
+bp = flask.blueprints.Blueprint('nodes', __name__, url_prefix='/nodes')
+
+
+@bp.route('/')
+@bp.route('/list')
 def list():
-    return web.node.get_node_names_and_namespaces()
+    all_nodes = web.node.get_node_names_and_namespaces()
+
+    # inspired on: https://github.com/ros2/ros2cli/blob/13870ad51cb8d52dd7171407793ae0198dcc6ac4/ros2node/ros2node/api/__init__.py#L65
+    def _node_name(name, namespace):
+        ns = namespace
+        if not namespace.endswith('/'):
+            ns += '/'
+        return ns + name
+    all_nodes = (_node_name(n, ns) for n, ns in all_nodes)
+    all_nodes = sorted(all_nodes)
+    return flask.render_template('nodes/list.html', nodes=all_nodes)
