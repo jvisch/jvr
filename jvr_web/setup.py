e48aaf745890e3bcc580175a4a241fe24bcfddf5
Jorg Visch <jorgvisch@gmail.com>
2023-02-22 14:56:25 +0100

    copying jinja templates

--- jvr_web/setup.py
+++ jvr_web/setup.py
@@ -1,11 +1,30 @@
 from setuptools import setup
+import os
 
 package_name = 'jvr_web'
+
+# copied from https://github.com/ros2/sros2/blob/bb3859098f123c65f8cfc90f45e21502be521d48/sros2/setup.py
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = (
+    package_files(f'{package_name}/templates')
+)
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    
+
+    package_data={
+        package_name : extra_files,
+    },
+
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
