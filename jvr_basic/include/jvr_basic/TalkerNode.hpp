bfc78c1bb739b1e332a457f0fd92fbff5996dad4
Jorg Visch <1082961+jvisch@users.noreply.github.com>
2023-12-16 16:58:07 +0100

    code formatted

--- jvr_basic/include/jvr_basic/TalkerNode.hpp
+++ jvr_basic/include/jvr_basic/TalkerNode.hpp
@@ -2,7 +2,6 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-
 class TalkerNode : public rclcpp::Node
 {
 public:
@@ -10,9 +9,8 @@ public:
 
 private:
   void timer_callback();
-  
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<jvr_interfaces::msg::TalkMsg>::SharedPtr publisher_;
   size_t count_;
 };
-
